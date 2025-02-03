a27ba45c94e9b164000301f9d8f00772df30a3b5
Opilane TTHK <opilane@tthk.ee>
2025-02-03 15:30:39 +0200

    init2

---
+++ IfElseTesting/IfElseColor/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace IfElseColor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teha if ja else konsoolirakendus, kus " +
+                "kontrollitakse stringi abil värvi vastavust");
+            Console.WriteLine("Värvide valikuks on: red, blue, green ja white");
+            Console.WriteLine("Sisestage värv:");
+
+            // küsi kasutajalt
+            string colorInput = Console.ReadLine().ToLower(); 
+
+
+            if (colorInput == "red" || colorInput == "blue" || colorInput == "green" || colorInput == "white")
+            {
+                Console.WriteLine($"Valisite värvi: {colorInput}");
+            }
+            else
+            {
+                // Handle invalid color input
+                Console.WriteLine("Vigane värv. Palun sisestage üks järgmistest värvidest: red, blue, green või white.");
+            }
+        }
+    }
+}
