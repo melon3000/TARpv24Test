86a8cb3dbbe29a204bebf0da9fcc364e015f2a39
Opilane TTHK <opilane@tthk.ee>
2025-02-03 14:48:33 +0200

    IfElseNesting1

--- IfElseTesting/IfElseTesting/Program.cs
+++ IfElseTesting/IfElseTesting/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Number Control!");
+            
+            int number = 201;
+
+            //Kontrollitakse kas arv on suurem 189-st
+            if (number > 189)
+            {
+                //kui vastab if tingimustele 
+                Console.WriteLine("Number is bigger");
+
+                //if ja else Nestimine 
+                if (number >= 190 && number < 200)
+                    Console.WriteLine("Number is between 190 to 200");
+                
+                else
+                {
+                    Console.WriteLine("Number is bigger than 200");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Number is smaller");
+            }
+
         }
     }
 }
