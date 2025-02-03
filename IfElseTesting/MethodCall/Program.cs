89a7f858b4c32c8679f74eb2b65f6fdcea19c130
Opilane TTHK <opilane@tthk.ee>
2025-02-03 16:39:33 +0200

    init4

---
+++ IfElseTesting/MethodCall/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MethodCall
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Main1();
+        }   
+        static void Main1()
+        {
+            Console.WriteLine("Metood 1");
+        }
+    }
+}
